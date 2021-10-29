2c02d2273e3275ed543fe7bff76102cf799e2430
prajakta <prajaktamahajan789@gmail.com>
2021-10-29 22:55:36 +0530

    Added:User need to enter valid email

--- src/main/java/org/bridgelabz/UserRegistration.java
+++ src/main/java/org/bridgelabz/UserRegistration.java
@@ -5,10 +5,7 @@ import java.util.regex.Pattern;
 
 	public class UserRegistration {
 
-	    /*
-	    Created a method to validate first name
-	     */
-	    public boolean firstName(String firstName) {
+		public boolean firstName(String firstName) {
 	        String regex = "^[A-Z]{1}[a-z]{2,}$";
 	        Pattern pattern = Pattern.compile(regex);
 	        Matcher matcher = pattern.matcher(firstName);
@@ -24,4 +21,15 @@ import java.util.regex.Pattern;
 	        Matcher matcher = pattern.matcher(lastName);
 	        return matcher.matches();
 	    }
+
+	    /*
+	    Created a method to validate E-mail
+	     */
+	    public boolean email(String email) {
+	        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+	        Pattern pattern = Pattern.compile(regex);
+	        Matcher matcher = pattern.matcher(email);
+	        return matcher.matches();
+	    }
 	}
+
