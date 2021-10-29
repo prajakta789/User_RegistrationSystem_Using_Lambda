c72a435c15952046a8f421092a46d87e02610f0d
prajakta <prajaktamahajan789@gmail.com>
2021-10-30 00:14:30 +0530

    Added:Lambda Expression

---
+++ src/main/java/org/bridgelabz/UserRegistrationException.java
@@ -0,0 +1,23 @@
+package org.bridgelabz;
+
+public class UserRegistrationException extends Exception {
+
+	
+	    String message;
+	    static ExceptionType type;
+
+	    enum ExceptionType {
+	        NULL, EMPTY;
+	    }
+
+	    public UserRegistrationException(ExceptionType type, String message) {
+	        this.type = type;
+	        this.message = message;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return message;
+	    }
+	}
+	
